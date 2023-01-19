5d2451ed99b3b30e047b5b158ea372a1f11e3f1e
Gabriel Henrique <bielriquecustodio@gmail.com>
2023-01-19 16:24:48 -0300

    Creation of the elements of the Home page and stylization of them as the project asks.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,18 @@
+import styles from "./Home.module.css"
+import savings from "../img/savings.svg"
+import LinkButton from "../layout/LinkButton";
+
 const Home = () => {
-    return ( <h1>Home</h1> );
-}
- 
+  return (
+    <section className={styles.homeContainer}>
+      <h1>
+        Bem-vindo ao <span>Costs</span>
+      </h1>
+      <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+      <LinkButton to="/newproject" text="Criar projeto"/>
+      <img src={savings} alt="savings" />
+    </section>
+  );
+};
+
 export default Home;
