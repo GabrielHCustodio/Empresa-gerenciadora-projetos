ea357bf7bcf338648017dc67c39df0222e86fc54
Gabriel Henrique <bielriquecustodio@gmail.com>
2023-01-20 23:39:37 -0300

    Added the elements that make up the NewProject page and performed the componentization of the form.

--- src/pages/NewProject.jsx
+++ src/pages/NewProject.jsx
@@ -1,5 +1,15 @@
+import ProjectForm from "../project/ProjectForm";
+
+import styles from "./NewProject.module.css"
+
 const NewProject = () => {
-    return ( <h1>New project</h1> );
+    return (
+        <div className={styles.newProjectContainer}>
+            <h1>Criar projeto</h1>
+            <p>Crie o projeto para depois adicionar os serviços</p>
+            <ProjectForm btnText="Criar projeto"/>
+        </div>
+    );
 }
  
 export default NewProject;
